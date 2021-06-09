9e09071fbbf2f52830b6f6915fc13ddba90fc0cd
serakoi <admin@seer-software.com>
2021-06-09 14:25:40 +0200

    Test

--- js/dist/admin.js
+++ js/dist/admin.js
@@ -114,8 +114,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var flarum_app__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(flarum_app__WEBPACK_IMPORTED_MODULE_0__);
 // Big thanks to: https://discuss.flarum.org/u/Justoverclock for helping!
 
-flarum_app__WEBPACK_IMPORTED_MODULE_0___default.a.initializers.add('serakoi-flarumdiscordtheme', function () {
-  flarum_app__WEBPACK_IMPORTED_MODULE_0___default.a.extensionData["for"]('serakoi-flarumdiscordtheme').registerSetting({
+flarum_app__WEBPACK_IMPORTED_MODULE_0___default.a.initializers.add('serakoi/flarum-discord-theme', function () {
+  flarum_app__WEBPACK_IMPORTED_MODULE_0___default.a.extensionData["for"]('serakoi/flarum-discord-theme').registerSetting({
     setting: 'serakoi-flarumdiscordtheme.authorbadgetext',
     name: 'authorBadgeText',
     type: 'text',
--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,8 +1,8 @@
 // Big thanks to: https://discuss.flarum.org/u/Justoverclock for helping!
 import app from 'flarum/app';
 
-app.initializers.add('serakoi-flarumdiscordtheme', () => {
-  app.extensionData.for('serakoi-flarumdiscordtheme').registerSetting({
+app.initializers.add('serakoi/flarum-discord-theme', () => {
+  app.extensionData.for('serakoi/flarum-discord-theme').registerSetting({
     setting: 'serakoi-flarumdiscordtheme.authorbadgetext',
     name: 'authorBadgeText',
     type: 'text',
