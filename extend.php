53c4fb5ed625f93361b446524b8418caa0749790
serakoi <admin@seer-software.com>
2021-06-09 13:51:50 +0200

    Test (I forgot to extend it)

--- extend.php
+++ extend.php
@@ -16,6 +16,8 @@ use Flarum\Extend;
 return [
     (new Extend\Frontend('forum'))
         ->css(__DIR__.'/resources/less/forum.less'),
+    (new Extend\Frontend('admin'))
+        ->js(__DIR__.'/js/dist/admin.js'),
     
     new Extend\Locales(__DIR__ . '/resources/locale')
 ];
